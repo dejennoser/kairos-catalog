06ce0156355748cd66be237baaf9e39ae9bd054c
dejennoser <dejen.teklit@noser.com>
2026-05-06 13:56:29 +0200

    feat: add ProductController

---
+++ src/main/java/com/kairos/catalog/controller/ProductController.java
@@ -0,0 +1,73 @@
+package com.kairos.catalog.controller;
+
+
+import com.kairos.catalog.dto.ProductRequest;
+import com.kairos.catalog.dto.ProductResponse;
+import com.kairos.catalog.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/products")
+@RequiredArgsConstructor
+@Tag(name = "products", description = "Product catalog management API")
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    @Operation(summary = "Get all products")
+    public ResponseEntity<List<ProductResponse>> findAll() {
+        return ResponseEntity.ok(productService.findAll());
+
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get a product by ID")
+    public ResponseEntity<ProductResponse> findById(@PathVariable @NonNull UUID id){
+        return ResponseEntity.ok(productService.findById(id));
+    }
+
+    @GetMapping("/category/{category}")
+    @Operation(summary = "Get a product by category")
+    public ResponseEntity<List<ProductResponse>> findByCategory(@PathVariable @NonNull String category){
+        return ResponseEntity.ok(productService.findByCategory(category));
+    }
+
+    @GetMapping("/search")
+    @Operation(summary = "Get a product by name")
+    public ResponseEntity<List<ProductResponse>> search(@RequestParam @NonNull String name){
+        return ResponseEntity.ok(productService.searchByName(name));
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a new product")
+    public ResponseEntity<ProductResponse> create(@RequestBody @Valid ProductRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(request));
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update an existing product")
+
+    public ResponseEntity<ProductResponse> update(
+            @PathVariable @NonNull UUID id,
+            @RequestBody @Valid ProductRequest request) {
+        return  ResponseEntity.ok(productService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete an existing product")
+    public ResponseEntity<Void> delete(@PathVariable @NonNull UUID id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
