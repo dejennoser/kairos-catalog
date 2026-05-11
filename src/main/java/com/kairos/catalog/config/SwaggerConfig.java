af6c0e9c9fc02fb28141fb5fb55203f8585f514d
dejennoser <dejen.teklit@noser.com>
2026-05-11 14:13:49 +0200

    feat: integrate Keycloak OAuth2 with role-based authorization and Swagger login
    - secure API using Keycloak as OAuth2 resource server
    - implement role-based access control (USER read, ADMIN write)
    - add custom JWT role extraction from realm_access
    - enable Swagger OAuth2 authorization code flow
    - support Keycloak login directly from Swagger UI

--- src/main/java/com/kairos/catalog/config/SwaggerConfig.java
+++ src/main/java/com/kairos/catalog/config/SwaggerConfig.java
@@ -4,25 +4,39 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    private static final String SCHEME_NAME = "bearerAuth";
+    private static final String SECURITY_SCHEME_NAME = "keycloak";
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(new Components()
-                        .addSecuritySchemes(SCHEME_NAME,
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
                                 new SecurityScheme()
-                                        .name("Authorization")
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
+                                        .type(SecurityScheme.Type.OAUTH2)
+                                        .description("Keycloak OAuth2")
+                                        .flows(new OAuthFlows()
+                                                .authorizationCode(new OAuthFlow()
+                                                        .authorizationUrl(
+                                                                "http://localhost:8180/realms/kairos/protocol/openid-connect/auth"
+                                                        )
+                                                        .tokenUrl(
+                                                                "http://localhost:8180/realms/kairos/protocol/openid-connect/token"
+                                                        )
+                                                        .scopes(new Scopes()
+                                                                .addString("openid", "OpenID Connect")
+                                                        )
+                                                )
+                                        )
                         )
                 );
     }
